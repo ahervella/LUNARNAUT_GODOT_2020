be6240db30edb78bdf2588c336f980bf02312fae
Alejandro <ahervella@me.com>
2020-06-12 16:11:00 -0500

    added pause menu to touch controls!

--- touchControls.gd
+++ touchControls.gd
@@ -43,7 +43,11 @@ var currAstroCharSelection = null
 
 
 
-var touchStateDict = {"stick" : TOUCH_STATE.NO_TOUCH, "jump" : TOUCH_STATE.NO_TOUCH, "interact" : TOUCH_STATE.NO_TOUCH, "cs" : TOUCH_STATE.NO_TOUCH}
+var touchStateDict = {"stick" : TOUCH_STATE.NO_TOUCH, 
+"jump" : TOUCH_STATE.NO_TOUCH, 
+"interact" : TOUCH_STATE.NO_TOUCH, 
+"cs" : TOUCH_STATE.NO_TOUCH, 
+"pause" : TOUCH_STATE.NO_TOUCH}
 
 
 var movingUp = -1
@@ -59,10 +63,45 @@ onready var touchStickInner = touchStick.get_children()[0]
 onready var interactTween = Tween.new()
 export (NodePath) var touchInteractPath = null
 onready var touchInteract = get_node(touchInteractPath)
+var touchInteractXBound = []
+var touchInteractYBound = []
+
+onready var pauseTween = Tween.new()
+export (NodePath) var touchPausePath = null
+onready var touchPause = get_node(touchPausePath)
+var pauseToggle = false
+var touchPauseWidth = 250
+var touchPauseHeight = 40
+var touchPauseXBound = []
+var touchPauseYBound = []
+
+export (NodePath) var touchPause_scanBGPath = null
+export (NodePath) var touchPause_settingsPath = null
+export (NodePath) var touchPause_mainMenuPath = null
+export (NodePath) var touchPause_reloadLvlPath = null
+export (NodePath) var touchPause_loadCPPath = null
+
+
+onready var touchPause_scanBG = get_node(touchPause_scanBGPath)
+onready var touchPause_settings = get_node(touchPause_settingsPath)
+onready var touchPause_loadCP = get_node(touchPause_loadCPPath)
+onready var touchPause_reloadLvl = get_node(touchPause_reloadLvlPath)
+onready var touchPause_mainMenu = get_node(touchPause_mainMenuPath)
+
+onready var touchPause_options = [touchPause_settings,
+ touchPause_loadCP,
+ touchPause_reloadLvl,
+ touchPause_mainMenu]
+
+var touchPause_opt_height = 160
+var touchPause_opt_width = 480
+onready var touchPause_refPoint = touchPause_settings.get_global_position()
 
 onready var csTween = Tween.new()
 export (NodePath) var touchCSPath = null
 onready var touchCS = get_node(touchCSPath)
+var touchCSXBound = [0]
+var touchCSYBound = [0]
 
 export (NodePath) var touchCS1973Path = null
 export (NodePath) var touchCS1984Path = null
@@ -101,7 +140,11 @@ func _ready():
 	touchButton.set_modulate(Color(1, 1, 1, 0))
 	touchStick.set_modulate(Color(1, 1, 1, 0))
 	touchInteract.set_modulate(Color(1, 1, 1, 0))
+	touchPause.set_modulate(Color(1, 1, 1, 0.25))
 	touchCS.set_modulate(Color(1, 1, 1, 0.25))
+	touchPause_scanBG.hide()
+	for touchPause_opt in touchPause_options:
+		touchPause_opt.hide()
 	
 	for value in touchCSDict.values():
 		value.set_modulate(Color(1, 1, 1, 0))
@@ -115,6 +158,8 @@ func _ready():
 	
 	add_child(stickTween)
 	#stickTween.interpolate_property(touchStick, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1 , 0, Tween.EASE_OUT, 0)
+	
+	add_child(pauseTween)
 	
 	add_child(interactTween)
 	#interactTween.interpolate_property(touchInteract, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1 , 0, Tween.EASE_OUT, 0)
@@ -149,40 +194,58 @@ func _input(event):
 		
 		if !(event is InputEventScreenDrag) && !event.is_pressed() && ongoing_cs_drag != -1:
 			print("blah")
+			
 		
-		if ((event.position.x < 150 && event.position.y < 140 && !buttonIsShowing) 
-		|| csButtonIsShowing 
-		&& (event is InputEventScreenDrag 
-			|| (event is InputEventScreenTouch && !event.is_pressed() 
-				&& ongoing_cs_drag != -1)  )):
-			joyCS(event)
+			
+			
+		if (event.position.x > touchPause.get_global_position().x - touchPauseWidth/2 
+		&& event.position.x < touchPause.get_global_position().x + touchPauseWidth/2
+		&& event.position.y < touchPause.get_global_position().y + touchPauseHeight/2
+		&& (event is InputEventScreenTouch || event is InputEventScreenDrag)):
+			joyPause(event)
 		
-		#if event is touch onto screen and left half of screen, 
-		#OR   if button is showing AND (drag event or meets condition to reset)
-		elif ((event.position.x < 640 
-			and event is InputEventScreenTouch 
-			and event.is_pressed()) 
-		or (buttonIsShowing 
-			and (event is InputEventScreenDrag 
-				or (event is InputEventScreenTouch 
-					and !event.is_pressed() 
-					and ongoing_drag != -1)))):
-			joyStick(event)
 		
-		var pos = event.position
 		
-		#if in the middle, it is an interact signal
-		if (canInteract
-		and event.position.x >= interactCenter.x - interactWidthHeight/2 
-			and event.position.x < interactCenter.x + interactWidthHeight/2
-		and event.position.y >= interactCenter.y - interactWidthHeight/2 
-			and event.position.y < interactCenter.y + interactWidthHeight/2
-		 and event is InputEventScreenTouch):
-			joyInteract(event)
-
-		#if on the right it is a jump signal
-		elif (event.position.x >= 640 and event is InputEventScreenTouch):
-			joyJump(event)
+		elif !pauseToggle:
+			#TODO: fix dragging over from jump button or anywhere else
+			if ((event.position.x < 150 && event.position.y < 140 && !buttonIsShowing) #&& ongoing_cs_drag != event.get_index()) 
+			|| csButtonIsShowing 
+			&& (event is InputEventScreenDrag 
+				|| (event is InputEventScreenTouch && !event.is_pressed() 
+					&& ongoing_cs_drag == event.get_index())  )):
+				joyCS(event)
+			
+			#if event is touch onto screen and left half of screen, 
+			#OR   if button is showing AND (drag event or meets condition to reset)
+			elif ((event.position.x < 640 
+				and event is InputEventScreenTouch 
+				and event.is_pressed()) 
+			or (buttonIsShowing 
+				and (event is InputEventScreenDrag 
+					or (event is InputEventScreenTouch 
+						and !event.is_pressed() 
+						and ongoing_drag != -1)))):
+				joyStick(event)
+			
+			var pos = event.position
+			
+			#if in the middle, it is an interact signal
+			if (canInteract
+			and event.position.x >= interactCenter.x - interactWidthHeight/2 
+				and event.position.x < interactCenter.x + interactWidthHeight/2
+			and event.position.y >= interactCenter.y - interactWidthHeight/2 
+				and event.position.y < interactCenter.y + interactWidthHeight/2
+			 and event is InputEventScreenTouch):
+				joyInteract(event)
+	
+			#if on the right it is a jump signal
+			elif (event.position.x >= 640 and event is InputEventScreenTouch):
+				joyJump(event)
+			
+			
+			
+		if pauseToggle:
+			joyPauseOptions(event)
 			
 			
 func setInteractAvailable(set: bool):
@@ -274,6 +337,32 @@ func showCSChar(astroChar, visible, selected):
 		tween.interpolate_property(node, "modulate", Color(1, 1, 1, currColor.a), Color(1, 1, 1, 0), 0.5 , 0, Tween.EASE_OUT, 0)
 	
 	tween.start()
+	
+func showPause(visible, toggled):
+	
+	var currColor = touchPause.get_modulate()
+	
+	if (visible):
+		touchPause.show()
+		#tween.stop(node, "modulate")
+		var alpha = 1 if toggled else 0.25
+		
+		pauseTween.interpolate_property(touchPause, "modulate", Color(1, 1, 1, currColor.a), Color(1, 1, 1, alpha), 0.25 , 0, Tween.EASE_OUT, 0)
+	
+		if toggled:
+			touchPause_scanBG.show()
+			for touchPause_opt in touchPause_options:
+				touchPause_opt.show()
+		else:
+			touchPause_scanBG.hide()
+			for touchPause_opt in touchPause_options:
+				touchPause_opt.hide()
+			
+	else:
+
+		pauseTween.interpolate_property(touchPause, "modulate", Color(1, 1, 1, currColor.a), Color(1, 1, 1, 0), 0.5 , 0, Tween.EASE_OUT, 0)
+	
+	pauseTween.start()
 
 func joyStick(event):
 	#if past deadzone radius (threshold), change direction of stick
@@ -359,8 +448,37 @@ func joyCS(event):
 		if currAstroCharSelection != null:
 			global.initCharSwitch(currAstroCharSelection)
 
+func joyPause(event):
+	if (event is InputEventScreenTouch and event.is_pressed()):
+		setTouchState("pause", TOUCH_STATE.JUST_TOUCHED)
+		pauseToggle = !pauseToggle
+		showPause(true, pauseToggle)
 
 
+func joyPauseOptions(event):
+	if (event is InputEventScreenTouch and event.is_pressed()):
+		if (event.position.x > touchPause_refPoint.x - touchPause_opt_width/2
+		&& event.position.x < touchPause_refPoint.x + touchPause_opt_width/2):
+			
+			var selectedOpt = null
+			var refY = touchPause_refPoint.y - touchPause_opt_height/2
+			for i in touchPause_options.size():
+				print(refY + (touchPause_opt_height * i))
+				if (event.position.y > refY + (touchPause_opt_height * i) 
+				&& event.position.y < refY + (touchPause_opt_height * (i+1))):
+				
+					
+					selectedOpt = i
+					break
+			
+			match(selectedOpt):
+				2:
+					global.goto_scene(global.lvl().filename)
+					continue
+				3:
+					global.replay()
+					continue
+	
 
 #set the touch state for the touch input
 func setTouchState(touchStateInput, touchState):
@@ -397,7 +515,6 @@ func joyJump(event):
 func joyInteract(event):
 	if !canInteract: return
 	if (event is InputEventScreenTouch and event.is_pressed()):
-		touchButton.global_position = event.position
 		setTouchState("interact", TOUCH_STATE.JUST_TOUCHED)
 		showInteract(true, true)
 	else:
